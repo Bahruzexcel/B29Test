d9a3c0f588a78fd62d551a3eb62d7b622ac840cd
Bahruzexcel <31067932+Bahruzexcel@users.noreply.github.com>
2023-06-27 17:56:39 -0400

    Update Test.java

--- src/main/java/com/example/b29test/Test.java
+++ src/main/java/com/example/b29test/Test.java
@@ -2,21 +2,5 @@ package com.example.b29test;
 
 public class Test {
     public static void main(String[] args) {
-        Car car1 =new Car();
-//        car1.color="black";
-//        System.out.println(car1.color);
-//        car1.color="red";
-        System.out.println("car1.getColor() = " + car1.getColor());
-        car1.setColor("yellow");
-        System.out.println("car1.getColor() = " + car1.getColor());
-        car1.setModel("tesla");
-        System.out.println("car1.toString() = " + car1.toString());
-        System.out.println(car1.getColor());
-
-    }
-}
-interface dogInterface{
-    public abstract void bark();
-
 
 }
